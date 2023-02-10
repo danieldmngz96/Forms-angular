4d69aac8519c6d1d3cf6fddecca77cd08b484a2a
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-02-10 01:01:18 -0500

    ejemplos de formfiel imputs dinamicos con Angular v10

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -8,10 +8,24 @@ import { FormControl } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl("soy un form control");
+  nameField = new FormControl('');
+  emailField = new FormControl('');
+  phoneField = new FormControl('');
+  dateField = new FormControl('');
+  numberField = new FormControl('');
+  colorField = new FormControl("#000");
+
   constructor() { }
 
   ngOnInit(): void {
+    this.nameField.valueChanges.subscribe((value: string) => {
+      console.log(value);
+    });
+    this.emailField.valueChanges.subscribe((value: string) => {
+      console.log(value);
+    })
   }
-
+ getNameValue() {
+  console.log(this.nameField.value);
+ }
 }
